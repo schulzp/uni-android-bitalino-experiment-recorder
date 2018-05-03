cca2c00ef5a5447e54dbe3b40224059b311c7268
Peter Schulz <schulzp@users.noreply.github.com>
2018-05-03 21:45:39 +0200

    initial commit

---
+++ app/src/main/java/uni/bremen/conditionrecorder/ListRecordingsFragment.kt
@@ -0,0 +1,18 @@
+package uni.bremen.conditionrecorder
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class ListRecordingsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list_recordings, container, false)
+    }
+}
