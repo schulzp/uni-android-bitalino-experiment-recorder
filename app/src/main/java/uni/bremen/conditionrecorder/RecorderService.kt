b8b8619ae1cff15cc17809a07218909fe38e040a
Peter Schulz <schulzp@users.noreply.github.com>
2018-05-07 22:12:06 +0200

    created bitalino recording session

--- app/src/main/java/uni/bremen/conditionrecorder/RecorderService.kt
+++ app/src/main/java/uni/bremen/conditionrecorder/RecorderService.kt
@@ -2,21 +2,25 @@ package uni.bremen.conditionrecorder
 
 import android.app.Activity
 import android.app.Service
+import android.bluetooth.BluetoothDevice
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.HandlerThread
 import android.os.IBinder
 import android.util.Log
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.zipWith
+import uni.bremen.conditionrecorder.bitalino.BITalinoRecordingSession
 import java.util.*
 
 class RecorderService : Service() {
 
-    val bus:RecorderBus = RecorderBus()
+    val bus: RecorderBus = RecorderBus()
 
     val bitalinoRecoringStared = bus.eventSubject.filter { it is RecorderBus.BitalinoRecordingStarted }.map { it as RecorderBus.BitalinoRecordingStarted }
     val bitalinoRecordingStopped = bus.eventSubject.filter { it is RecorderBus.BitalinoRecordingStopped }.map { it as RecorderBus.BitalinoRecordingStopped }
@@ -31,14 +35,18 @@ class RecorderService : Service() {
 
     private val binder = Binder()
 
-    private val disposables:MutableMap<String, Disposable> = HashMap()
+    private val disposables: MutableMap<String, Disposable> = HashMap()
+
+    private val handlerThread = HandlerThread(TAG)
+
+    private fun looper():Scheduler = AndroidSchedulers.from(handlerThread.looper)
 
     override fun onBind(intent: Intent): IBinder = binder
 
     override fun onCreate() {
         super.onCreate()
 
-        disposables["commands"] = bus.commandSubject.subscribeOn(AndroidSchedulers.mainThread()).subscribe {
+        disposables["commands"] = bus.commandSubject.subscribeOn(looper()).subscribe {
             when (it) {
                 is RecorderBus.StartRecording -> startRecording()
                 is RecorderBus.StopRecording -> stopRecording()
@@ -49,31 +57,44 @@ class RecorderService : Service() {
     override fun onDestroy() {
         super.onDestroy()
 
+        bitalinoRecordingSession?.close()
+
         disposables.values.forEach { it.dispose() }
         disposables.clear()
     }
 
+    fun startSession(bluetoothDevice: BluetoothDevice) {
+        bitalinoRecordingSession = BITalinoRecordingSession(this, bluetoothDevice)
+    }
+
+    private var bitalinoRecordingSession: BITalinoRecordingSession? = null
+
     private fun startRecording() {
         Log.d(TAG, "started recording")
+
+        bitalinoRecordingSession?.start()
     }
 
     private fun stopRecording() {
         Log.d(TAG, "stopped recording")
+
+        bitalinoRecordingSession?.close()
     }
+
 
     inner class Binder : android.os.Binder() {
 
-        fun getService():RecorderService = this@RecorderService
+        fun getService(): RecorderService = this@RecorderService
 
     }
 
     class Connection : ServiceConnection {
 
-        private var service:RecorderService? = null
+        private var service: RecorderService? = null
 
         private val disposables = LinkedList<Disposable>()
 
-        private val queue = LinkedList<(connection:Connection, service:RecorderService) -> Any>()
+        private val queue = LinkedList<(connection: Connection, service: RecorderService) -> Any>()
 
         override fun onServiceConnected(source: ComponentName, binder: IBinder) {
             if (binder is RecorderService.Binder) {
@@ -126,14 +147,15 @@ class RecorderService : Service() {
 
         const val TAG = "RecorderService"
 
-        fun bind(context: Context, listener: ((connection: Connection, service: RecorderService) -> Unit)? = null):Connection {
+        fun bind(context: Context, listener: ((connection: Connection, service: RecorderService) -> Unit)? = null): Connection {
             return Connection()
-                    .also { connection ->  context.bindService(
-                            Intent(context, RecorderService::class.java),
-                            connection,
-                            Context.BIND_AUTO_CREATE)
+                    .also { connection ->
+                        context.bindService(
+                                Intent(context, RecorderService::class.java),
+                                connection,
+                                Context.BIND_AUTO_CREATE)
                     }
-                    .also { connection ->  if (listener != null) connection.whenConnected(listener) }
+                    .also { connection -> if (listener != null) connection.whenConnected(listener) }
         }
 
     }
---
+++ app/src/main/java/uni/bremen/conditionrecorder/bitalino/BITalinoRecordingSession.kt
@@ -0,0 +1,121 @@
+package uni.bremen.conditionrecorder.bitalino
+
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Parcelable
+import android.util.Log
+import info.plux.pluxapi.Communication
+import info.plux.pluxapi.Constants
+import info.plux.pluxapi.bitalino.*
+import info.plux.pluxapi.bitalino.bth.OnBITalinoDataAvailable
+import uni.bremen.conditionrecorder.RecorderDeviceFragment
+import uni.bremen.conditionrecorder.RecorderService
+
+class BITalinoRecordingSession(val context: Context, val bluetoothDevice: BluetoothDevice) {
+
+    private var bitalino: BITalinoCommunication? = null
+
+    private val dataReceiver: OnBITalinoDataAvailable = object : OnBITalinoDataAvailable {
+
+        override fun onBITalinoDataAvailable(bitalinoFrame: BITalinoFrame) {
+            Log.d(RecorderService.TAG, "frame available: $bitalinoFrame")
+        }
+
+    }
+
+    private val updateReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (Constants.ACTION_STATE_CHANGED == action) {
+                val identifier = intent.getStringExtra(Constants.IDENTIFIER)
+                val state = info.plux.pluxapi.Constants.States.getStates(intent.getIntExtra(Constants.EXTRA_STATE_CHANGED, 0))
+
+                Log.i(RecorderService.TAG, identifier + " -> " + state!!.name)
+
+                when (state) {
+                    Constants.States.NO_CONNECTION -> {
+                    }
+                    Constants.States.LISTEN -> {
+                    }
+                    Constants.States.CONNECTING -> {
+                    }
+                    Constants.States.CONNECTED -> {
+                    }
+                    Constants.States.ACQUISITION_TRYING -> {
+                    }
+                    Constants.States.ACQUISITION_OK -> {
+                    }
+                    Constants.States.ACQUISITION_STOPPING -> {
+                    }
+                    Constants.States.DISCONNECTED -> {
+                    }
+                    Constants.States.ENDED -> {
+                    }
+                }
+            } else if (Constants.ACTION_DATA_AVAILABLE == action) {
+                if (intent.hasExtra(Constants.EXTRA_DATA)) {
+                    val parcelable = intent.getParcelableExtra<Parcelable>(Constants.EXTRA_DATA)
+                    if (parcelable.javaClass == BITalinoFrame::class.java) {
+                        val frame = parcelable as BITalinoFrame
+                        handleFrame(frame)
+                    }
+                }
+            } else if (Constants.ACTION_COMMAND_REPLY == action) {
+                val identifier = intent.getStringExtra(Constants.IDENTIFIER)
+
+                if (intent.hasExtra(Constants.EXTRA_COMMAND_REPLY) && intent.getParcelableExtra<Parcelable>(Constants.EXTRA_COMMAND_REPLY) != null) {
+                    val parcelable = intent.getParcelableExtra<Parcelable>(Constants.EXTRA_COMMAND_REPLY)
+                    if (parcelable.javaClass == BITalinoState::class.java) {
+                        Log.d(RecorderDeviceFragment.TAG, (parcelable as BITalinoState).toString())
+                    } else if (parcelable.javaClass == BITalinoDescription::class.java) {
+                        var isBITalino2 = (parcelable as BITalinoDescription).isBITalino2
+                        Log.d(RecorderService.TAG, "isBITalino2: " + isBITalino2 + "; FwVersion: " + parcelable.fwVersion.toString())
+                    }
+                }
+            }
+        }
+
+        fun handleFrame(frame: BITalinoFrame) {
+            Log.d(RecorderService.TAG, "frame: $frame")
+        }
+    }
+
+    fun start() {
+        var communication = Communication.getById(bluetoothDevice!!.type)
+
+        Log.d(RecorderService.TAG, "communication: ${communication.name}")
+
+        if (communication == Communication.DUAL) {
+            communication = Communication.BLE
+        }
+
+        bitalino = BITalinoCommunicationFactory().getCommunication(communication, context, dataReceiver)
+
+        context.registerReceiver(updateReceiver, makeUpdateIntentFilter())
+    }
+
+    fun close() {
+        context.unregisterReceiver(updateReceiver)
+
+        try {
+            bitalino?.closeReceivers()
+            bitalino?.disconnect()
+        } catch (e: BITalinoException) {
+            Log.e(RecorderService.TAG, "failed to close/disconnect from bitalino", e)
+        }
+    }
+
+    private fun makeUpdateIntentFilter(): IntentFilter {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(Constants.ACTION_STATE_CHANGED)
+        intentFilter.addAction(Constants.ACTION_DATA_AVAILABLE)
+        intentFilter.addAction(Constants.ACTION_EVENT_AVAILABLE)
+        intentFilter.addAction(Constants.ACTION_DEVICE_READY)
+        intentFilter.addAction(Constants.ACTION_COMMAND_REPLY)
+        return intentFilter
+    }
+
+}
